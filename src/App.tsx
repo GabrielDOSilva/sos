ed050976b30480970bc5aeef3fdb9f214ce98e6f
Gabriel <gabriel.silva781.09@gmail.com>
2023-02-17 22:02:39 -0300

    Feat: implementação de tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { ThemeProvider } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
-import { AppRoutes } from "./routes/routes";
+import { AppRoutes } from "./routes/Routes";
+import { LightTheme } from "./shrades/themes";
 
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
