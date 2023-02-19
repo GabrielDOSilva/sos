5fbf0691facdf92b525f3a7ea20482ad3735a53a
Gabriel <gabriel.silva781.09@gmail.com>
2023-02-18 22:10:28 -0300

    Feat: Adicionado responsividade ao menu lateral

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes/Routes";
 import { Sidebar } from "./shrades/components";
-import { AppThemeProvider } from "./shrades/contexts";
+import { AppThemeProvider, DrawerProvider } from "./shrades/contexts";
 
 
 
 export const App = () => {
   return (
     <AppThemeProvider>
-      <BrowserRouter>
-        
-        <Sidebar>
-        <AppRoutes />
-        </Sidebar>
-        
-      </BrowserRouter>
+      <DrawerProvider>
+        <BrowserRouter>
+          <Sidebar>
+            <AppRoutes />
+          </Sidebar>
+        </BrowserRouter>
+      </DrawerProvider>
     </AppThemeProvider>
   );
 }
--- src/shrades/components/sidebar/drawer.tsx
+++ src/shrades/components/sidebar/drawer.tsx
@@ -1,22 +1,47 @@
-import { Drawer, useTheme } from "@mui/material";
+import { Drawer, useTheme, Avatar, Divider, List, ListItemIcon, ListItemText } from "@mui/material";
+import HomeIcon from '@mui/icons-material/Home';
 import { Box } from "@mui/system";
+import ListItemButton from "@mui/material/ListItemButton";
+import useMediaQuery from "@mui/material/useMediaQuery/useMediaQuery";
+import { useDrawerContext } from "../../contexts";
+
 
 interface SidebarProps {
     children: React.ReactNode;
-}
+
+};
+
 export const Sidebar: React.FC<SidebarProps> = ({ children }) => {
     const theme = useTheme();
+    const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+    
+    const  {isDrawerOpen, toggleDrawerOpen}  = useDrawerContext( );
 
     return (
         <>
-            <Drawer variant='permanent'>
-                <Box width={theme.spacing(28)}>
-                teste
+            <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'permanent'} onClose={toggleDrawerOpen}>
+                <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column">
+                    <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
+                        <Avatar
+                            sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
+                            src="https://avatars.githubusercontent.com/u/99222405?v=4" />
+                    </Box>
+                    <Divider />
+                    <Box flex={1}>
+                        <List component="nav">
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <HomeIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Home" />
+                            </ListItemButton>
+                        </List>
+                    </Box>
                 </Box>
             </Drawer>
-            <Box height='100vh' marginLeft={theme.spacing(28)}>
+            <Box height='100vh' marginLeft={smDown ? 0 : theme.spacing(28)}>
                 {children}
             </Box>
-        </>
+            </>
     );
 };
